303b41c465806efc8a0bac4f1aa50d5cfbc6d272
Hrittik Bhattacharjee <100524273+BhattacharjiHRB@users.noreply.github.com>
2024-11-19 01:26:39 +0600

    bug fixed

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,22 +1,20 @@
-
-"use client"
-import React, { FormEvent, useState } from 'react';
-import { motion } from 'framer-motion';
-import sendEmail from '@/actions/sendEmail';
+"use client";
+import sendEmail from "@/actions/sendEmail";
+import { motion } from "framer-motion";
+import { FormEvent, useState } from "react";
 
 const ContactPage = () => {
-  const [email, setEmail] = useState<string>('');
-  const [message, setMessage] = useState<string>('');
+  const [email, setEmail] = useState<string>("");
+  const [message, setMessage] = useState<string>("");
   const [success, setSuccess] = useState<boolean>(false);
   const [error, setError] = useState<boolean>(false);
-
 
   const handleFormSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     const formData = new FormData();
-    formData.append('senderEmail', email);
-    formData.append('message', message);
+    formData.append("senderEmail", email);
+    formData.append("message", message);
 
     try {
       const { data } = await sendEmail(formData);
@@ -29,11 +27,11 @@ const ContactPage = () => {
   const textMessage = "Contact Me";
 
   return (
-    <div className='h-full'>
-      <div className='flex flex-col lg:flex-row h-full p-4 sm:p-8 md:p-12 lg:p-20 xl:p-48'>
+    <div className="h-full">
+      <div className="flex flex-col lg:flex-row h-full p-4 sm:p-8 md:p-12 lg:p-20 xl:p-48">
         <div className="h-1/2 lg:h-full lg:w-1/2 flex justify-center items-center gap-2 mt-10 text-2xl md:text-3xl lg:text-4xl xl:text-6xl">
           <div>
-            {textMessage.split('').map((letter, index) => (
+            {textMessage.split("").map((letter, index) => (
               <motion.span
                 key={index}
                 initial={{ opacity: 1 }}
@@ -43,7 +41,7 @@ const ContactPage = () => {
                   duration: 3,
                   repeat: Infinity,
                 }}
-                className='text-purple-50'
+                className="text-purple-50"
               >
                 {letter}
               </motion.span>
@@ -53,38 +51,46 @@ const ContactPage = () => {
         </div>
         <form
           onSubmit={handleFormSubmit}
-          className='h-1/2 lg:h-full lg:w-1/2 flex flex-col bg-slate-950/80 rounded text-sm lg:text-xl gap-4 justify-center p-10 lg:p-24'
+          className="h-1/2 lg:h-full lg:w-1/2 flex flex-col bg-slate-950/80 rounded text-sm lg:text-xl gap-4 justify-center p-10 lg:p-24"
         >
           <label htmlFor="senderEmail">Email Address</label>
           <input
             id="senderEmail"
-            name='senderEmail'
-            type='email'
+            name="senderEmail"
+            type="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             required
-            className=' bg-transparent border-b-2 border-purple-100 focus:outline-none'
+            className=" bg-transparent border-b-2 border-purple-100 focus:outline-none"
             placeholder="yourname@gmail.com"
           />
           <label htmlFor="message">Dear Hrittik,</label>
           <textarea
             id="message"
-            name='message'
+            name="message"
             value={message}
             onChange={(e) => setMessage(e.target.value)}
             rows={10}
             required
-            className=' bg-transparent border-b-2 border-purple-100 focus:outline-none'
+            className=" bg-transparent border-b-2 border-purple-100 focus:outline-none"
             placeholder="write me a message"
           />
           <button
-            type='submit'
-            className='bg-purple-900 hover:bg-purple-300 hover:text-purple-950 py-2 mt-5'
+            type="submit"
+            className="bg-purple-900 hover:bg-purple-300 hover:text-purple-950 py-2 mt-5"
           >
             Send
           </button>
-          {success && <div className='bg-emerald-800/80 text-white text-center text-xl py-1 '>Email sent successfully!</div>}
-          {error && <div className='bg-rose-800/80 text-white text-center text-xl py-1 '>Can't send due to free domain</div>}
+          {success && (
+            <div className="bg-emerald-800/80 text-white text-center text-xl py-1 ">
+              Email sent successfully!
+            </div>
+          )}
+          {error && (
+            <div className="bg-rose-800/80 text-white text-center text-xl py-1 ">
+              Can&apos; send due to free domain
+            </div>
+          )}
         </form>
       </div>
     </div>
